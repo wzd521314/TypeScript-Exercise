55b9ec277ae61b9e3c1f218fea719314dc1500ed
Kugou <565078415@qq.com>
2020-08-28 10:32:52 +0800

    基本语法学习完成

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-let test : number = 123;
+class Person {
+  private name:string;
+  protected sex:string
+  age:number;
+  constructor(name:string, age:number, sex:string) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+}
 
-console.log(test);
+
+class My extends Person {
+  constructor( name:string, age:number, sex:string) {
+    super(name,age,sex)
+  }
+  
+}
+
+let test = new My('张三',23,"男")
+let test1 = new Person('张三',23,"男")
+
+console.log(test1.name)
